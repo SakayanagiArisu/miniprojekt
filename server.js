bef9409874ae6f244b3004ee3e38573bfa2361c0
Artur <arturro.solarro@gmail.com>
2020-12-06 11:44:23 +0100

    commit kur**ę

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
     res.send("<h1>first app on heroku!</h1>")
@@ -9,3 +9,4 @@ app.get("/", function (req, res) {
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
+
