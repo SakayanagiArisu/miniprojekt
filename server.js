b709d501230848457d3315ad758c8e7ccf002e74
Artur <arturro.solarro@gmail.com>
2020-12-06 11:36:01 +0100

    commit on master

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-//zmienne, stałe
-
-var express = require("express");
-var app = express();
-const PORT = process.env.PORT || 3000; 
-
-//funkcje na serwerze obsługujące konkretne adresy w przeglądarce
-var path = require("path");
+var express = require("express")
+var app = express()
+var PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
-    res.send('działa')
+    res.send("<h1>first app on heroku!</h1>")
 })
 
-app.use(express.static('static'));
-
-//nasłuch na określonym porcie
-
-app.listen(PORT, function () { 
-    console.log("start serwera na porcie " + PORT );
+app.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT)
 })
